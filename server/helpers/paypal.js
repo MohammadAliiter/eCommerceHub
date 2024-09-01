4caa5f85222bbd5b80184b1c7259ef012c9a8b14
Mohammad Ali <123582111+MohammadAliiter@users.noreply.github.com>
2024-09-01 18:30:20 +0530

    Changes

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,8 +1,9 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: process.env.PAYPAL_MODE,
+  mode: process.env.PAYPAL_MODE, // 'sandbox' ya 'live'
   client_id: process.env.PAYPAL_CLIENT_ID,
-  client_secret: process.env.PAYPAL_CLIENT_SECRET,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET
 });
+
 module.exports = paypal;
